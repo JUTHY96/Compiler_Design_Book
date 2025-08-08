d5dad177893574f7aed663dccff77ab03fc666c4
JUTHY96 <juthyfarzanaakter0@gmail.com>
2025-08-09 01:12:17 +0600

    First and Follow

---
+++ chapter 8/problem1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0;
+    printf("Enter a string: ");
+    gets(str);
+    while (str[i] != '\0') {
+        i++;
+    }
+    printf("Length of string is %d\n", i);
+    return 0;
+}
